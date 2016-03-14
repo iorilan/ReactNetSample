0f6572c1b0f125c26b00c59b901f7e479d5e0767
lanliang <iorilan@hotmail.com>
2016-03-14 19:04:45 +0800

    init

---
+++ ReactNetSample/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactNetSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private static List<CommentModel> _comments;
+
+        public ActionResult Comments()
+        {
+            if (_comments.Count == 0)
+            {
+                _comments = new List<CommentModel>
+                {
+                    new CommentModel
+                    {
+                        Author = "Daniel Lo Nigro",
+                        Text = "Hello ReactJS.NET World!"
+                    },
+                    new CommentModel
+                    {
+                        Author = "Pete Hunt",
+                        Text = "This is one comment"
+                    },
+                    new CommentModel
+                    {
+                        Author = "Jordan Walke",
+                        Text = "This is *another* comment"
+                    },
+                };
+            }
+            return Json(_comments ,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AddComment(CommentModel comment)
+        {
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+    }
+
+    public class CommentModel
+    {
+        public string Author { get; set; }
+        public string Text { get; set; }
+    }
+}
